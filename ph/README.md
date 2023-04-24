da41a83882ec1313147f16fedb4e0dbad8322c38
Yuxi Long <yl708@cs.duke.edu>
2023-04-24 08:17:20 -0400

    added documentation for redis

--- ph/README.md
+++ ph/README.md
@@ -5,9 +5,17 @@ Currently a fork of code provided in *Representability of algebraic topology for
 ## This folder: Calculates the persistent homology of PDB files
 
 Workflow:
-1.  Make a folder elsewhere. Download and extract the *refined set* and the *index files* from [PDBBind](http://www.pdbbind.org.cn/download.php). In the folder for *refined set*, delete the non-PDB folders. Find them via `ls -l | grep -E -v '\s[0-9a-z]{4}$'`.
-2. Download [pdb2pqr v2.1.0](https://github.com/Electrostatics/pdb2pqr/releases/tag/v2.1.0) and use `utils/make_pqrs.py` to convert things to `.pqr`.
-3.
+1.  **Downloading and preprocessing the PDBBind dataset**:
+    1. Make a folder elsewhere. Download and extract the *refined set* and the *index files* from [PDBBind](http://www.pdbbind.org.cn/download.php).
+    2. In the folder for *refined set*, delete the non-PDB folders. Find them via `ls -l | grep -E -v '\s[0-9a-z]{4}$'`.
+    3. **Preprocessing the Download [pdb2pqr v2.1.0](https://github.com/Electrostatics/pdb2pqr/releases/tag/v2.1.0) and use `utils/make_pqrs.py` to convert things to `.pqr`.
+2. **Starting a Redis database**: The Redis database is used for managing jobs submitted to the SLURM batch cluster.
+    1. Make and install Redis via [https://redis.io/docs/getting-started/installation/install-redis-from-source/].
+    2. Optionally, add the `src` folder of Redis to path.
+    3. Create a `redis.conf` file somewhere and set a default password by putting e.g. `requirepass topology` in that file.
+    4. Start the redis server on a host and adjust the `DB` constant in `dispatch_jobs.py` accordingly.
+3. **Test run**: Test run with `test_example.py`. If successful, the folder being operated upon should end up with 20 files. No more, no less.
+4. **Full run**: `dispatch_jobs.py`
 
 
 
--- ph/dispatch_jobs.py
+++ ph/dispatch_jobs.py
@@ -18,12 +18,12 @@ CLUSTER = 'CS' # or 'DCC'
 if CLUSTER == 'CS':
     cwd = pathlib.Path(__file__).parent.resolve()
     CSV_FILE = f'{cwd}/jobs.csv'
-    NUM_JOBS_TO_SUBMIT = 5000
+    NUM_JOBS_TO_SUBMIT = 300
     PYTHON_EXECUTABLE = '/usr/project/dlab/Users/jaden/mambaforge/envs/algtop-ph/bin/python'
     ROOT_DIR = '/usr/project/dlab/Users/jaden/algebraic-topology-biomolecules/ph'
     os.system(f'mkdir -p {ROOT_DIR}/slurm-outs')
     SBATCH_TEMPLATE = f"""#!/bin/bash
-#SBATCH --time='4:00:00'
+#SBATCH --time='24:00:00'
 #SBATCH --requeue
 #SBATCH --chdir={ROOT_DIR}
 #SBATCH --output={ROOT_DIR}/slurm-outs/%x-%j-slurm.out
@@ -93,6 +93,7 @@ def main():
         if info['finished'] == 'True' and info['error'] == 'False':
             continue
         else:
+            i += 1
             # submit job for it
             info['attempted'] = 'True'
             DB.hset(get_db_key(folder), mapping=info)
@@ -106,6 +107,8 @@ def main():
 
             os.system(f'sbatch --job-name={get_name(folder)} run.sh')
 
+    print(f'Number of jobs submitted: {i}')
+
 def populate_db():
     # For each key, add its default entry to the database if it does not exist
     list(map(lambda folder: DB.hset(get_db_key(folder), mapping={
@@ -116,32 +119,38 @@ def populate_db():
         'error': 'False'
     }), filter(lambda folder: not DB.exists(get_db_key(folder)), FOLDERS)))
 
-# def remake_df(csv_file):
-#     # Remakes df based on the folder
-#     print('Remaking csv file')
-#     os.system(f'rm {csv_file}')
-#     df = get_df(csv_file)
-#     n_fin = 0
-#     n_err = 0
-#     for idx in range(len(df)):
-#         folder_name = df.at[idx, 'folder']
-#         if len(glob.glob(folder_name + '/*')) == 6:
-#             df.at[idx, 'attempted'] = False
-#             df.at[idx, 'finished'] = False
-#             df.at[idx, 'error'] = False
-#         elif len(glob.glob(folder_name + '/*')) == 20:
-#             df.at[idx, 'attempted'] = True
-#             df.at[idx, 'finished'] = True
-#             df.at[idx, 'error'] = False
-#             n_fin += 1
-#         else:
-#             df.at[idx, 'attempted'] = True
-#             df.at[idx, 'finished'] = False
-#             df.at[idx, 'error'] = True
-#             n_err += 1
+def rebuild_db():
+    n_fin = 0
+    n_err = 0
+    for folder in FOLDERS:
+        if len(glob.glob(folder + '/*')) == 6:
+            DB.hset(get_db_key(folder), mapping={
+                'name': get_name(folder),
+                'folder': folder,
+                'attempted': 'False',
+                'finished': 'False',
+                'error': 'False'
+            })
+        elif len(glob.glob(folder + '/*')) == 20:
+            DB.hset(get_db_key(folder), mapping={
+                'name': get_name(folder),
+                'folder': folder,
+                'attempted': 'True',
+                'finished': 'True',
+                'error': 'False'
+            })
+            n_fin += 1
+        else:
+            DB.hset(get_db_key(folder), mapping={
+                'name': get_name(folder),
+                'folder': folder,
+                'attempted': 'True',
+                'finished': 'False',
+                'error': 'True'
+            })
+            n_err += 1
 
-#     df.to_csv(csv_file, index=False)
-#     print(f'Number finished: {n_fin}; number erred: {n_err}; number not started: {len(df) - n_fin - n_err}')
+    print(f'Rebuilding database: Number finished: {n_fin}; number erred: {n_err}; number not started: {len(FOLDERS) - n_fin - n_err}')
 
 
 def get_db():
@@ -149,4 +158,5 @@ def get_db():
     return DB
 
 if __name__ == '__main__':
+    rebuild_db()
     main()
